02a5d9f8fe06075bd4d8957eaf90543aaeb3e4d7
Habibullo Hamidov <h.hamidov77777@gmail.com>
2024-09-19 19:48:40 +0900

    feat: publish main layout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,20 @@
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
-import { Box, Container, Stack } from "@mui/material";
-import { brown, green } from "@mui/material/colors";
+import { Stack } from "@mui/material";
 import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <Stack>
-      <Stack flexDirection={"column"}>
+    <Stack className={"home-page"}>
+      <Stack>
         <Stack className="container">Popular Properties</Stack>
-        <Box>Top Agents</Box>
-        <Box>Popular Properties</Box>
-        <Box>Events</Box>
+      </Stack>
+
+      <Stack>
+        <Stack className="container">Top Agents</Stack>
+      </Stack>
+
+      <Stack>
+        <Stack className="container">Top Properties</Stack>
       </Stack>
     </Stack>
   );
